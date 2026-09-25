5c2945dca65ca51135f2d1d1b354a3c2655eba71
Raul Mazo <raulmazo@gmail.com>
2026-09-24 21:58:22 -0300

    feat(chapter6): add exs.pl for snippet 6.3

---
+++ chapter6/exs.pl
@@ -0,0 +1,21 @@
+% Positive examples - graspable objects 
+
+pos(graspable(obj1)). 
+
+pos(graspable(obj2)). 
+
+pos(graspable(obj3)). 
+
+ 
+
+% Negative examples - non-graspable objects 
+
+neg(graspable(obj4)). 
+
+neg(graspable(obj5)). 
+
+neg(graspable(obj6)). 
+
+neg(graspable(obj7)). 
+
+neg(graspable(obj8)). 
