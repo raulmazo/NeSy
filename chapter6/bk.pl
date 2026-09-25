c73fd6cdd5c607dcc259466928ecbd24b76e3f37
Raul Mazo <raulmazo@gmail.com>
2026-09-24 21:56:13 -0300

    feat(chapter6): add bk.pl for snippet 6.2

---
+++ chapter6/bk.pl
@@ -0,0 +1,57 @@
+% Shape facts 
+
+shape(obj1, s_cube). 
+
+shape(obj2, s_sphere). 
+
+shape(obj3, s_cylinder). 
+
+shape(obj4, s_irregular). 
+
+shape(obj5, s_cube). 
+
+shape(obj6, s_sphere). 
+
+shape(obj7, s_irregular). 
+
+shape(obj8, s_cylinder). 
+
+ 
+
+% Weight facts 
+
+weight(obj1, w_light). 
+
+weight(obj2, w_light). 
+
+weight(obj3, w_light). 
+
+weight(obj4, w_heavy). 
+
+weight(obj5, w_heavy). 
+
+weight(obj6, w_heavy). 
+
+weight(obj7, w_light). 
+
+weight(obj8, w_heavy). 
+
+ 
+
+% Shape type predicates 
+
+cube(s_cube). 
+
+sphere(s_sphere). 
+
+cylinder(s_cylinder). 
+
+irregular(s_irregular). 
+
+ 
+
+% Weight type predicates 
+
+light(w_light). 
+
+heavy(w_heavy). 
